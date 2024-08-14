45e55cb22d74db3cef7c90b2036ac3839863b0fd
Michal Blažek <blaza0@email.cz>
2024-08-14 19:13:16 +0200

    v1.23

--- app/tasks/_components/FilterBar.tsx
+++ app/tasks/_components/FilterBar.tsx
@@ -6,10 +6,7 @@ import { useState } from "react"
 const FilterBar = ({ inputValue }) => {
 
   return (
-    <div className="mx-8 py-2 bg-gray-500/20 rounded-2xl border-customColor1 border-2 ">
-
-
-      <form className="max-w-sm mx-auto">
+      <form className="max-w-sm mx-10 ">
         <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Hledej</label>
         <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -17,12 +14,12 @@ const FilterBar = ({ inputValue }) => {
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
             </svg>
           </div>
-          <input onChange={(e) => inputValue(e.target.value)} type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 rounded-lg" placeholder="Vyhledej inzerát" required />
+          <input onChange={(e) => inputValue(e.target.value)} type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border-black border-2 rounded-lg" placeholder="Vyhledej inzerát" required />
 
         </div>
       </form>
 
-    </div>
+
 
   )
 }
