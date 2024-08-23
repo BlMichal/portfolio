72fd4eff226aea3ec95d9e68539a1cab329266d2
Michal Blažek <blaza0@email.cz>
2024-08-23 13:16:08 +0200

    v1.47 Loading

--- app/advertisement/_components/FilteredAdView.tsx
+++ app/advertisement/_components/FilteredAdView.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import FilterBar from "./FilterBar";
 import AdsView from "./AdCards";
 import AdCardsLayoutButton from "./AdCardsLayoutButton";
+import { LoaderCircle } from "lucide-react";
 
 const FilteredAdView = ({ advertisementData }:{advertisementData : AdProps[]|null}) => {
 
@@ -12,7 +13,14 @@ const FilteredAdView = ({ advertisementData }:{advertisementData : AdProps[]|nul
   const [selectedCategory, setSelectedCategory] = useState<string>("");
   const [layout, setLayout] = useState<string>("layout2");
 
-
+  if(!advertisementData){
+    return(
+    <div className="max-w-7xl mx-auto flex items-center justify-center gap-2 flex-auto">
+      <span><LoaderCircle size={50} className="animate-spin" /></span>
+      <h2 className="">Načítáme data...</h2>
+    </div>
+    )
+  }
 
   function removeAccents(str: string) {
     return str
@@ -24,10 +32,7 @@ const FilteredAdView = ({ advertisementData }:{advertisementData : AdProps[]|nul
   const handleFilterChange = (newFilterValue: string, newCategory: string) => {
     setInputValue(newFilterValue);
     setSelectedCategory(newCategory);
-
-    if(!advertisementData){
-      return null
-    }
+    
 
     const filtered = advertisementData.filter(({title, desc, category}) => {
       const titleMatch = removeAccents(title).includes(removeAccents(newFilterValue));
@@ -41,15 +46,15 @@ const FilteredAdView = ({ advertisementData }:{advertisementData : AdProps[]|nul
   };
 
   return (
-    <div className="max-w-7xl mx-auto flex-auto">
-      <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-2">
-      <FilterBar inputValue={(value) => handleFilterChange(value, selectedCategory)}
-        selectValue={(value) => handleFilterChange(inputValue, value)} />
-      <AdCardsLayoutButton setLayout={setLayout} />
-      </div>
-      <AdsView filteredAds={filteredAds} layout={layout} />
+     <div className="max-w-7xl mx-auto flex-auto">
+       <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-2">
+       <FilterBar inputValue={(value) => handleFilterChange(value, selectedCategory)}
+       selectValue={(value) => handleFilterChange(inputValue, value)} />
+       <AdCardsLayoutButton setLayout={setLayout} />
+       </div>
+       <AdsView filteredAds={filteredAds} layout={layout} />
     </div>
-  );
+   );
 };
 
 export default FilteredAdView;
--- app/advertisement/page.tsx
+++ app/advertisement/page.tsx
@@ -1,25 +1,37 @@
 import AsideBar from "@/components/AsideBar";
 import { createClient } from "@/utils/supabase/server";
 import FilteredAdView from "./_components/FilteredAdView";
+import { Suspense } from "react";
+import { LoaderCircle } from "lucide-react";
 
 export default async function TaskPage() {
-
   const supabase = createClient();
 
   const { data: advertisement, error } = await supabase
     .from("TabAdvertisement")
     .select("*, TabAdsImages(imageUrl)")
-    .order('id', { ascending: false })
-    .returns<AdProps[]>()
+    .order("id", { ascending: false })
+    .returns<AdProps[]>();
 
-  if(error){
+  if (error) {
     throw new Error(`Chyba při načítání dat ${error.message}`);
   }
 
   return (
     <section className="bg-peak-background md:min-h-screen-content-md-footer min-h-screen-content-sm-footer flex">
       <AsideBar />
-      <FilteredAdView advertisementData={advertisement} />
+      <Suspense
+        fallback={
+          <div className="max-w-7xl mx-auto flex items-center justify-center gap-2 flex-auto">
+            <span>
+              <LoaderCircle size={50} className="animate-spin" />
+            </span>
+            <h2 className="">Načítáme data...</h2>
+          </div>
+        }
+      >
+        <FilteredAdView advertisementData={advertisement} />
+      </Suspense>
     </section>
   );
 }
